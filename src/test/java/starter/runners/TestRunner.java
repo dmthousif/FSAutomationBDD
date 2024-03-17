e2ffc88c12be4cdcf126856cbaf981631beca8e6
Thousif_Danki <thousif.danki@macys.com>
2024-03-17 11:16:56 +0530

    Added BDD Code

---
+++ src/test/java/starter/runners/TestRunner.java
@@ -0,0 +1,18 @@
+package starter.runners;
+
+
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        plugin = {"pretty"},
+        features = {"src/test/resources/features"},
+        glue = "starter",
+        tags = "not @pending"
+)
+public class TestRunner {
+}
+
+
